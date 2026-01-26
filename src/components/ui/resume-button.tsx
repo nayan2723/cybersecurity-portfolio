c5adc52bca5329c52fa322fadc735588ab36ffaf
Nayan Kshitij <nayankshitij128@gmail.com>
2026-01-26 20:00:56 +0530

    Update resume-button.tsx

--- src/components/ui/resume-button.tsx
+++ src/components/ui/resume-button.tsx
@@ -20,7 +20,7 @@ const ResumeButton = ({
   text = 'View Resume'
 }: ResumeButtonProps) => {
   const handleResumeClick = () => {
-    window.open('/Nayan_resume_final.pdf', '_blank');
+    window.open('public/Nayan_resume_final.pdf.pdf', '_blank');
   };
 
   const IconComponent = iconType === 'download' ? Download : FileText;
